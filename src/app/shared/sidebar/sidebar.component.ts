3d605cef5e726978bf76a5386555aef1fed1f78f
Jon Herrera <jherrera@opentrends.net>
2023-03-15 15:34:15 +0100

    Commit03 - Sidebar funcional

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -2,8 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html'
+  templateUrl: './sidebar.component.html',
+  styles: [
+    `
+    li {
+      cursor: pointer;
+    }
+    `
+  ]
 })
+
 export class SidebarComponent {
 
 }
