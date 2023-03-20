1fbb2cc9177e0c9809b5fa8c18c4d0c34ce4aa3c
Jon Herrera <jherrera@opentrends.net>
2023-03-20 12:08:42 +0100

    Commit07 - Input y tabla busqueda por países

---
+++ src/app/pais/components/pais-tabla/pais-tabla.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PorPaisComponent } from '../../pages/por-pais/por-pais.component';
+
+@Component({
+  selector: 'app-pais-tabla',
+  templateUrl: './pais-tabla.component.html',
+})
+export class PaisTablaComponent {
+
+  @Input() paises: Country[] = [];
+
+  constructor() {
+
+  }
+
+  
+}
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { Country, Name } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
@@ -14,9 +14,10 @@ export class PorPaisComponent {
 
   constructor( private paisService: PaisService ) { }
 
-  buscar() {
+  buscar( termino: string ) {
+
     this.hayError = false;
-    console.log(this.termino);
+    this.termino = termino;
 
     this.paisService.buscarPais( this.termino )
       .subscribe( (paises) => {
@@ -28,6 +29,11 @@ export class PorPaisComponent {
         this.paises   = [];
       });
 
+  }
+
+  sugerencias( termino: string ) {
+    this.hayError = false;
+    // TODO: Crear sugerencias
   }
 
 }
