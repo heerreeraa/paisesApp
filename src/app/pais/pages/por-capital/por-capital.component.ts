06770625bfcf01f3926e9e6a8327bbcf4669518a
Jon Herrera <jherrera@opentrends.net>
2023-03-22 16:25:54 +0100

    Commit08 - Componente buscarPais completo

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
-  templateUrl: './por-capital.component.html',
-  styleUrls: ['./por-capital.component.css']
+  templateUrl: './por-capital.component.html'
 })
 export class PorCapitalComponent {
 
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor( private paisService: PaisService ) { }
+
+  buscar( termino: string ) {
+
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarCapital( this.termino )
+      .subscribe( (paises) => {
+        this.paises = paises;        
+      }, (err) => {
+        this.hayError = true;
+        this.paises   = [];
+      });
+  }
 }
--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+ 
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
-  templateUrl: './ver-pais.component.html',
-  styleUrls: ['./ver-pais.component.css']
+  templateUrl: './ver-pais.component.html'
 })
-export class VerPaisComponent {
+export class VerPaisComponent implements OnInit {
 
+  pais!: Country[];
+
+  constructor( 
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+    ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({ id }) => this.paisService.getPaisPorAlpha( id ) ),
+        tap( console.log )
+      )
+      .subscribe( pais => this.pais = pais );
+      console.log(this.pais);
+      
+  }
 }
